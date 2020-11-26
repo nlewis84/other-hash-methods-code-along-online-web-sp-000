0a1dd8030a9de5b5acabc914191b75356e2073f7
Nathan Lewis <n.lewis.perc@gmail.com>
2020-11-26 04:26:17 +0000

    Automatically backed up by Learn

--- lib/values.rb
+++ lib/values.rb
@@ -12,7 +12,7 @@ def get_the_values(groceries)
   #code your solution here!
   values = []
   groceries.each do |groups, foods|
-    values << groups.values
+    values << groceries.values
   end
   values
 end
